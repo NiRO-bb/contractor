ea6a2011ab26edd88c6f1f4731f45b008b79e89e
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-07-04 09:44:44 +0300

    test: update tests
    Add application-test.properties

--- src/test/java/com/example/Contractor/ContractorRepositoryTest.java
+++ src/test/java/com/example/Contractor/ContractorRepositoryTest.java
@@ -13,10 +13,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class ContractorRepositoryTest {
 
     @Autowired
