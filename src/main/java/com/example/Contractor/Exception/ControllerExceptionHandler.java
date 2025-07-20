9db4b6783c7e1268968539127b2163249473254e
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-07-20 16:35:07 +0400

    feat: update ControllerExceptionHandler

--- src/main/java/com/example/Contractor/Exception/ControllerExceptionHandler.java
+++ src/main/java/com/example/Contractor/Exception/ControllerExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * Handles exceptions from controller classes.
  */
-@RestControllerAdvice("package com.example.Contractor.Controller")
+@RestControllerAdvice
 public class ControllerExceptionHandler {
 
     /**
