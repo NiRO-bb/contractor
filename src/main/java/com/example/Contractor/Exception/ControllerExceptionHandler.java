4dd5acbe8b83f598fe6c2a181d275cb127cbb5ea
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-07-21 12:27:57 +0400

    fix: update ControllerExceptionHandler
    handle default controllers only

--- src/main/java/com/example/Contractor/Exception/ControllerExceptionHandler.java
+++ src/main/java/com/example/Contractor/Exception/ControllerExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.example.Contractor.Exception;
 
+import com.example.Contractor.Controller.ContractorControllerImpl;
+import com.example.Contractor.Controller.CountryControllerImpl;
+import com.example.Contractor.Controller.IndustryControllerImpl;
+import com.example.Contractor.Controller.OrgFormControllerImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,7 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * Handles exceptions from controller classes.
  */
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = {ContractorControllerImpl.class,
+        CountryControllerImpl.class, IndustryControllerImpl.class, OrgFormControllerImpl.class})
 public class ControllerExceptionHandler {
 
     /**
