3ea7db887b05a5361ee0d9c6611230049be717e7
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-07-21 12:24:18 +0400

    feat: add controller interfaces
    add interfaces that must be implemented by default controllers

--- src/main/java/com/example/Contractor/Controller/ContractorController.java
+++ src/main/java/com/example/Contractor/Controller/ContractorController.java
@@ -2,198 +2,47 @@ package com.example.Contractor.Controller;
 
 import com.example.Contractor.DTO.Contractor;
 import com.example.Contractor.DTO.ContractorSearch;
-import com.example.Contractor.DTO.Country;
-import com.example.Contractor.DTO.Industry;
-import com.example.Contractor.DTO.OrgForm;
-import com.example.Contractor.Service.ContractorService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
- * Handles incoming http-requests related with Contractor.
- * <p>
- * Provides access to service layer with some CRUD-methods.
+ * Provides interface for Contractor controller class.
+ * Contains basic CRUD operations and search method with filtering and pagination.
  */
-@RestController
-@RequestMapping("/contractor")
-@RequiredArgsConstructor
-public class ContractorController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ContractorController.class);
-
-    private final ContractorService service;
+public interface ContractorController {
 
     /**
-     * Responsible for creating and updating {@code Contractor} entities.
-     * <p>
-     * Receives {@link Contractor} instance as request body.
-     * Throw RuntimeException if something goes wrong -
-     * it is assumed that it will be catched by global exception handler.
+     * Adds and updates Contractor entities.
      *
-     * @param contractor instance that must be saved or updated (by {@code id} field) in database
-     * @return added/updated {@code Contractor} instance and http OK status, or INTERNAL_SERVER_ERROR
+     * @param contractor instance that must be saved
+     * @return added/updated Contractor entity
      */
-    @Operation(summary = "Add/update Contractor",
-            description = "Adds or updates (depends on passed 'id' value) Contractor entity")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Contractor saved",
-                    content = @Content(schema = @Schema(implementation = Contractor.class))),
-            @ApiResponse(responseCode = "500", description = "Contractor saving was failed",
-                    content = @Content(schema = @Schema(type = "string", example = "error message")))
-    })
-    @PutMapping("/save")
-    public ResponseEntity<?> save(@RequestBody Contractor contractor) {
-        try {
-            Optional<?> optContractor = service.save(contractor);
-            if (optContractor.isPresent()) {
-                LOGGER.info("Contractor added {}", contractor.desc());
-                return new ResponseEntity<>(optContractor.get(), HttpStatus.OK);
-            } else {
-                LOGGER.error("Contractor not added {}", contractor.desc());
-                return new ResponseEntity<>("Contractor adding/updating was failed.",
-                        HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        } catch (Exception exception) {
-            LOGGER.error("Contractor adding/updating was failed; Error occurred {}", exception.getMessage());
-            throw new RuntimeException("Contractor adding/updating was failed.");
-        }
-    }
+    ResponseEntity<?> save(Contractor contractor);
 
     /**
-     * Responsible for providing {@code Contractor} instance from database.
-     * <p>
-     * Provides all related data (from {@link com.example.Contractor.DTO.Country},
-     * {@link com.example.Contractor.DTO.Industry} and {@link com.example.Contractor.DTO.OrgForm} also).
-     * Throw RuntimeException if something goes wrong -
-     * it is assumed that it will be catched by global exception handler.
+     * Retrieves Contractor entity by passed id value.
      *
-     * @param id value of {@code id} field of {@link Contractor} instance
-     * @return found {@code Contractor} instance with related data and http OK status;
-     * NOT_FOUND if there is no matched Contractor entity
+     * @param id value of id field of Contractor entity that must be retrieved
+     * @return matched Contractor entity
      */
-    @Operation(summary = "Retrieve some Contractor", description = "Retrieves Contractor entity by id")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Contractor received",
-                    content = @Content(array = @ArraySchema(schema =
-                    @Schema(oneOf = {Contractor.class, Country.class, Industry.class, OrgForm.class})))),
-            @ApiResponse(responseCode = "404", description = "Contractor not received",
-                    content = @Content(schema = @Schema(type = "string", example = "error message"))),
-            @ApiResponse(responseCode = "500", description = "Contractor receiving was failed",
-                    content = @Content(schema = @Schema(type = "string", example = "error message")))
-    })
-    @GetMapping("/{id}")
-    public ResponseEntity<?> get(@PathVariable String id) {
-        try {
-            Optional<?> contractorInfo = service.get(id);
-            if (contractorInfo.isPresent()) {
-                LOGGER.info("Contractor obtained {}", String.format("{ \"id\":\"%s\" }", id));
-                return new ResponseEntity<>(contractorInfo.get(), HttpStatus.OK);
-            } else {
-                LOGGER.warn("Contractor not obtained {}; There is no contractor with such id",
-                        String.format("{ \"id\":\"%s\" }", id));
-                return new ResponseEntity<>("There is no contractor with such ID.", HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception exception) {
-            LOGGER.error("Contractor obtaining was failed; Error occurred {}", exception.getMessage());
-            throw new RuntimeException("Contractor obtaining was failed.");
-        }
-    }
+    ResponseEntity<?> get(String id);
 
     /**
-     * Responsible for deleting {@code Contractor} instance from database.
-     * <p>
-     * Throw RuntimeException if something goes wrong -
-     * it is assumed that it will be catched by global exception handler.
+     * Deletes Contractor entity by passed id value.
      *
-     * @param id value of {@code id} field of {@link Contractor} instance
-     * @return http NO_CONTENT status (success); NOT_FOUND (there is no matched Contractor entity)
+     * @param id value of id field of Contractor entity that must be deleted
+     * @return NO_CONTENT response code if successful, NOT_FOUND else
      */
-    @Operation(summary = "Delete Contractor", description = "Logically deletes contractor entity")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Contractor deleted"),
-            @ApiResponse(responseCode = "404", description = "Contractor not deleted - invalid data",
-                    content = @Content(schema = @Schema(type = "string", example = "error message"))),
-            @ApiResponse(responseCode = "500", description = "Contractor deleting was failed",
-                    content = @Content(schema = @Schema(type = "string", example = "error message")))
-    })
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable String id) {
-        try {
-            if (service.delete(id) > 0) {
-                LOGGER.info("Contractor deleted {}", String.format("{ \"id\":\"%s\" }", id));
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            } else {
-                LOGGER.error("Contractor not deleted {}; There is no contractor with such id",
-                        String.format("{ \"id\":\"%s\" }", id));
-                return new ResponseEntity<>("Contractor deleting was failed. There is no contractor with such ID.",
-                        HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception exception) {
-            LOGGER.error("Contractor deleting was failed; Error occurred {}", exception.getMessage());
-            throw new RuntimeException("Contractor deleting was failed.");
-        }
-    }
+    ResponseEntity<?> delete(String id);
 
     /**
-     * Responsible for providing {@code Contractor} instances with sorting and paging.
-     * <p>
-     * Receives {@link ContractorSearch} instance as request body.
-     * Throw RuntimeException if something goes wrong -
-     * it is assumed that it will be catched by global exception handler.
+     * Retrieves Contactor entities by passed parameters.
+     * Results will be partitioned as pages.
      *
-     * @param contractorSearch contains the sorting fields
-     * @param page number of result page that will be returned
-     * @param size amount of returned {@link Contractor} entities
-     * @return {@code Contractor} instances that match the given condition (in {@code contactorSearch});
-     * returned instances count will be no more than page size
+     * @param search contains filtering fields
+     * @param page number of returning page
+     * @param size size (amount of Contractor entities) of page
+     * @return matched Contactor entities
      */
-    @Operation(summary = "Retrieve Contractor list based on passed parameters")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Contractor list retrieved",
-                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = Contractor.class)))),
-            @ApiResponse(responseCode = "204", description = "Contractor list not retrieved - there are no matched entities"),
-            @ApiResponse(responseCode = "500", description = "Contractor searching was failed",
-                    content = @Content(schema = @Schema(type = "string", example = "error message")))
-    })
-    @PostMapping("/search")
-    public ResponseEntity<?> search(
-            @RequestBody ContractorSearch contractorSearch,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
-        try {
-            List<Contractor> contractors = service.search(contractorSearch, page, size);
-            if (!contractors.isEmpty()) {
-                LOGGER.info("Contractor list obtained {}", String.format("{ \"count\":%d }", contractors.size()));
-                return new ResponseEntity<>(contractors, HttpStatus.OK);
-            } else {
-                LOGGER.info("Contractor list not obtained { \"count\":0 }");
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-        } catch (Exception exception) {
-            LOGGER.error("Contractor searching was failed; Error occurred {}", exception.getMessage());
-            throw new RuntimeException("Contractor searching was failed.");
-        }
-    }
+    ResponseEntity<?> search(ContractorSearch search, int page, int size);
 
 }
--- src/main/java/com/example/Contractor/Controller/IndustryController.java
+++ src/main/java/com/example/Contractor/Controller/IndustryController.java
@@ -1,178 +1,43 @@
 package com.example.Contractor.Controller;
 
-import com.example.Contractor.DTO.Country;
 import com.example.Contractor.DTO.Industry;
-import com.example.Contractor.Service.IndustryService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
- * Handles incoming http-requests related with Industry.
- * <p>
- * Provides access to service layer with some CRUD-methods.
+ * Provides interface for Industry controller class.
+ * Contains basic CRUD methods.
  */
-@RestController
-@RequestMapping("/contractor/industry")
-@RequiredArgsConstructor
-public class IndustryController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(IndustryController.class);
-
-    private final IndustryService service;
+public interface IndustryController {
 
     /**
-     * Responsible for providing active {@code Industry} instances.
-     * <p>
-     * Throw RuntimeException if something goes wrong -
-     * it is assumed that it will be catched by global exception handler.
+     * Retrieves all active Industry entities.
      *
-     * @return list of active (value of {@code is_active} field = true) {@code Industry} instances and http OK status
+     * @return matched Industry entities
      */
-    @Operation(summary = "Retrieve all Industries", description = "Retrieves all active Industry entities")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Industry list received",
-                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = Country.class)))),
-            @ApiResponse(responseCode = "204", description = "Industry list not received - there are no matched entities"),
-            @ApiResponse(responseCode = "500", description = "Industry list receiving was failing",
-                    content = @Content(schema = @Schema(type = "string", example = "error message")))
-    })
-    @GetMapping("/all")
-    public ResponseEntity<?> get() {
-        try {
-            List<Industry> list = service.get();
-            if (!list.isEmpty()) {
-                LOGGER.info("Industry list obtained {}", String.format("{ \"count\":%d }", list.size()));
-                return new ResponseEntity<>(list, HttpStatus.OK);
-            } else {
-                LOGGER.warn("Industry list not obtained { \"count\":0 }");
-                return new ResponseEntity<>("Industry list is empty.", HttpStatus.OK);
-            }
-        } catch (Exception exception) {
-            LOGGER.error("Industry list obtaining was failed; Error occurred {}", exception.getMessage());
-            throw new RuntimeException("Industry list obtaining was failed");
-        }
-    }
+    ResponseEntity<?> get();
 
     /**
-     * Responsible for providing {@code Industry} instance from database.
-     * <p>
-     * Throw RuntimeException if something goes wrong -
-     * it is assumed that it will be catched by global exception handler.
+     * Retrieves Industry entity by passed id value.
      *
-     * @param id value of {@code id} field of {@code Industry} instance
-     * @return {@code Industry} instance and OK status;
-     * NOT_FOUND - there is no matched Industry entities
+     * @param id value of id field of Industry entity that must be retrieved
+     * @return matched Industry entity
      */
-    @Operation(summary = "Retrieve Industry", description = "Retrieves Industry by id")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Industry received",
-                    content = @Content(schema = @Schema(implementation = Country.class))),
-            @ApiResponse(responseCode = "404", description = "Industry not received - invalid data",
-                    content = @Content(schema = @Schema(type = "string", example = "error message"))),
-            @ApiResponse(responseCode = "500", description = "Industry receiving was failing",
-                    content = @Content(schema = @Schema(type = "string", example = "error message")))
-    })
-    @GetMapping("/{id}")
-    public ResponseEntity<?> get(@PathVariable("id") int id) {
-        try {
-            Optional<Industry> optIndustry = service.get(id);
-            if (optIndustry.isPresent()) {
-                LOGGER.info("Industry obtained {}", optIndustry.get().desc());
-                return new ResponseEntity<>(optIndustry, HttpStatus.OK);
-            } else {
-                LOGGER.info("Industry not obtained {}; There is no Industry with such id", String.format(" \"id\":\"%s\" ", id));
-                return new ResponseEntity<>("There is no Industry with such id.", HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception exception) {
-            LOGGER.error("Industry obtaining was failed; Error occurred {}", exception.getMessage());
-            throw new RuntimeException("Industry obtaining was failed");
-        }
-    }
+    ResponseEntity<?> get(int id);
 
     /**
-     * Responsible for creating and updating {@code Industry} instances.
-     * <p>
-     * Throw RuntimeException if something goes wrong -
-     * it is assumed that it will be catched by global exception handler.
+     * Adds and updates Industry entity.
      *
-     * @param industry instance that must be added or updated in database
-     * @return added/updated Industry entity and OK status; INTERNAL_SERVER_ERROR if something goes wrong
+     * @param industry instance that must be saved
+     * @return added/updated Industry entity
      */
-    @Operation(summary = "Add/update Industry",
-            description = "Adds or updates (depends on passed 'id' value) Industry entity")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Industry saved",
-                    content = @Content(schema = @Schema(implementation = Country.class))),
-            @ApiResponse(responseCode = "500", description = "Industry not saved",
-                    content = @Content(schema = @Schema(type = "string", example = "error message")))
-    })
-    @PutMapping("/save")
-    public ResponseEntity<?> save(@RequestBody Industry industry) {
-        try {
-            Optional<Industry> optIndustry = service.save(industry);
-            if (optIndustry.isPresent()) {
-                LOGGER.info("Industry added {}", industry.desc());
-                return new ResponseEntity<>(optIndustry, HttpStatus.OK);
-            } else {
-                LOGGER.error("Industry not added {}", industry.desc());
-                return new ResponseEntity<>("Industry adding/updating was failed.", HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        } catch (Exception exception) {
-            LOGGER.error("Industry adding was failed; Error occurred {}", exception.getMessage());
-            throw new RuntimeException("Industry adding/updating was failed");
-        }
-    }
+    ResponseEntity<?> save(Industry industry);
 
     /**
-     * Responsible for deleting {@code Industry} instance from database.
-     * <p>
-     * Throw RuntimeException if something goes wrong -
-     * it is assumed that it will be catched by global exception handler.
+     * Logically deletes Industry entity by passed id value.
      *
-     * @param id value of {@code id} field of {@code Industry} instance
-     * @return NO_CONTENT - success, NOT_FOUND - there is no matched Industry entities
+     * @param id value of id field of Industry entity that must be deleted
+     * @return NO_CONTENT response code if successful, NOT_FOUND else
      */
-    @Operation(summary = "Delete Industry", description = "Logically deletes Industry entity")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Industry deleted"),
-            @ApiResponse(responseCode = "404", description = "Industry not deleted - invalid data",
-                    content = @Content(schema = @Schema(type = "string", example = "error message"))),
-            @ApiResponse(responseCode = "500", description = "Industry deleting was failed",
-                    content = @Content(schema = @Schema(type = "string", example = "error message")))
-    })
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") int id) {
-        try {
-            if (service.delete(id) > 0) {
-                LOGGER.info("Industry deleted {}", String.format(" \"id\":\"%s\" ", id));
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            } else {
-                LOGGER.warn("Industry not deleted {}; There is no Industry with such id", String.format(" \"id\":\"%s\" ", id));
-                return new ResponseEntity<>("Industry deleting was failed. There is no Industry with such ID.", HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception exception) {
-            LOGGER.error("Industry deleting was failed; Error occurred {}", exception.getMessage());
-            throw new RuntimeException("Industry deleting was failed");
-        }
-    }
+    ResponseEntity<?> delete(int id);
 
 }
