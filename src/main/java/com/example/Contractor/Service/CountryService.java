ba7adf23ef56e8040e6e9189c5ca734a25e88a7e
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-08-25 11:25:58 +0400

    feat: add country caching
    add new service for caching
    
    update default service to work with cache

--- src/main/java/com/example/Contractor/Service/CountryService.java
+++ src/main/java/com/example/Contractor/Service/CountryService.java
@@ -2,7 +2,9 @@ package com.example.Contractor.Service;
 
 import com.example.Contractor.DTO.Country;
 import com.example.Contractor.Repository.CountryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.Contractor.Service.Cached.CountryCachedService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,56 +14,55 @@ import java.util.Optional;
  * Couples controller-layer to repository-layer.
  */
 @Service
+@RequiredArgsConstructor
 public class CountryService {
+
+    private final CountryCachedService cachedService;
 
     private final CountryRepository repository;
 
     /**
-     * Creates instance with initialized {@code CountryRepository}.
-     *
-     * @param repository repository that will handle requests
-     * @see CountryRepository
-     */
-    @Autowired
-    public CountryService(CountryRepository repository) {
-        this.repository = repository;
-    }
-
-    /**
-     * Provides access to get-all method of repository-layer.
+     * Returns list of all active Country entities from cache.
      *
      * @return list of active (value of {@code is_active} field = true) {@code Country} instances
      */
     public List<Country> get() {
-        return repository.get();
+        return cachedService.get();
     }
 
     /**
-     * Provides access to get method of repository-layer.
+     * Returns Country entity from cache with passed 'id' value.
      *
      * @param id value of {@code id} field of {@link Country} instance
      * @return found {@code Country} instance or Optional.empty()
      */
     public Optional<Country> get(String id) {
-        return repository.get(id);
+        return cachedService.get()
+                .stream()
+                .filter(country -> country.getId().equals(id))
+                .findAny();
     }
 
     /**
-     * Provides access to save method of repository-layer.
+     * Saves Country instance in repository.
+     * Also makes cache (with key - countries::all) outdated.
      *
      * @param country instance that must be added or updated
      * @return added/updated Country instance or Optional.empty()
      */
+    @CacheEvict(value = "countries", key = "'all'")
     public Optional<Country> save(Country country) {
         return repository.save(country);
     }
 
     /**
-     * Provides access to delete method of repository-layer.
+     * Logically deletes Country entity in repository.
+     * Also makes cache (with key - countries::all) outdated.
      *
      * @param id value of {@code id} field of {@link Country} instance
      * @return deleted rows amount
      */
+    @CacheEvict(value = "countries", key = "'all'")
     public int delete(String id) {
         return repository.delete(id);
     }
