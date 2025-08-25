45ebd9fc32232ce88db4dccca109c18e73358b60
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-08-25 11:50:57 +0400

    feat: add industry caching
    add new service for caching
    update default service to work with cache

--- src/main/java/com/example/Contractor/Service/IndustryService.java
+++ src/main/java/com/example/Contractor/Service/IndustryService.java
@@ -2,7 +2,9 @@ package com.example.Contractor.Service;
 
 import com.example.Contractor.DTO.Industry;
 import com.example.Contractor.Repository.IndustryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.Contractor.Service.Cached.IndustryCachedService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,56 +14,55 @@ import java.util.Optional;
  * Couples controller-layer to repository-layer.
  */
 @Service
+@RequiredArgsConstructor
 public class IndustryService {
+
+    private final IndustryCachedService cachedService;
 
     private final IndustryRepository repository;
 
     /**
-     * Creates instance with initialized {@code IndustryRepository}.
-     *
-     * @param repository repository that will handle requests
-     * @see IndustryRepository
-     */
-    @Autowired
-    public IndustryService(IndustryRepository repository) {
-        this.repository = repository;
-    }
-
-    /**
-     * Provides access to get-all method of repository-layer.
+     * Returns list of all active Industry entities from cache.
      *
      * @return list of active (value of {@code is_active} field = true) {@code Industry} instances
      */
     public List<Industry> get() {
-        return repository.get();
+        return cachedService.get();
     }
 
     /**
-     * Provides access to get method of repository-layer.
+     * Returns Industry entity from cache with passed 'id' value.
      *
      * @param id value of {@code id} field of {@link Industry} instance
      * @return found {@code Industry} instance or Optional.empty()
      */
     public Optional<Industry> get(int id) {
-        return repository.get(id);
+        return cachedService.get()
+                .stream()
+                .filter(industry -> industry.getId() == id)
+                .findAny();
     }
 
     /**
-     * Provides access to save method of repository-layer.
+     * Saves Industry instance in repository.
+     * Also makes cache (with key - industries::all) outdated.
      *
      * @param industry instance that must be added or updated
      * @return added/updated Industry instance or Optional.empty()
      */
+    @CacheEvict(value = "industries", key = "'all'")
     public Optional<Industry> save(Industry industry) {
         return repository.save(industry);
     }
 
     /**
-     * Provides access to delete method of repository-layer.
+     * Logically deletes Industry entity in repository.
+     * Also makes cache (with key - industries::all) outdated.
      *
      * @param id value of {@code id} field of {@link Industry} instance
      * @return deleted rows amount
      */
+    @CacheEvict(value = "industries", key = "'all'")
     public int delete(int id) {
         return repository.delete(id);
     }
